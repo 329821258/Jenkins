6300f332d900c3fb27ee5ef6aebe22aa8a5cb170
wsx <329821258@qq.com>
2021-06-01 19:38:51 +0800

    First Test!

--- src/main/java/com/jenkins/controller/TestController.java
+++ src/main/java/com/jenkins/controller/TestController.java
@@ -8,6 +8,6 @@ package com.jenkins.controller;
 
 public class TestController {
     public static void main(String[] args) {
-        System.out.println("1");
+        System.out.println("a1");
     }
 }
