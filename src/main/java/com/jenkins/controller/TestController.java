e37dc84f77fb2bb9ab3a4100e4f876b9bfb76cef
wsx <329821258@qq.com>
2021-06-02 16:46:33 +0800

    First Test!

--- src/main/java/com/jenkins/controller/TestController.java
+++ src/main/java/com/jenkins/controller/TestController.java
@@ -9,6 +9,6 @@ package com.jenkins.controller;
 public class TestController {
     public static void main(String[] args) throws InterruptedException {
         Thread.sleep(300000);
-        System.out.println("a11111");
+        System.out.println("a111111");
     }
 }
